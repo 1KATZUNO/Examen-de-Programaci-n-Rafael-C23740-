65dc19e3bce825cf398fab05c2bf146c912f8897
1KATZUNO <147660938+1KATZUNO@users.noreply.github.com>
2024-06-17 07:43:25 -0600

    ohhhh yeahhhh

--- Paneles_Graficos/Pantalla_Principal.java
+++ Paneles_Graficos/Pantalla_Principal.java
@@ -1,98 +1,262 @@
+
+
 package Paneles_Graficos;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-public class Pantalla_Principal extends JFrame {
-
-    // Declarar e inicializar componentes
-    private JPanel panelPrincipal;
-    private JButton botonVerLugares;
-    private JButton botonPlanearActividades;
-    private JButton botonInfo;
-    private JButton botonNuestraFauna;
-
+public class Pantalla_Principal extends javax.swing.JFrame {
+    private javax.swing.JButton botonEdit;
+    private javax.swing.JButton botonInfo;
+    private javax.swing.JButton botonMenuUsuario;
+    private javax.swing.JButton botonNuestraFauna;
+    private javax.swing.JButton botonPlanearActividades;
+    private javax.swing.JButton botonVerLugares;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JPanel jPanel1;
+   
     public Pantalla_Principal() {
-        super("Nuestro Costa Rica");
+        initComponents();
+        this.setLocationRelativeTo(null);
+    }
 
-        // Inicializar componentes
-        panelPrincipal = new JPanel();
-        botonVerLugares = new JButton("VER LUGARES");
-        botonPlanearActividades = new JButton("PLANEA ACTIVIDADES");
-        botonInfo = new JButton("INFORMACIÖN");
-        botonNuestraFauna = new JButton("NUESTRA FAUNA"); // Renombrado y actualizado el texto
+   
+                             
+    private void initComponents() {
 
-        // Establecer layout para el panel principal
-        panelPrincipal.setLayout(new GridLayout(2, 2, 20, 20)); // 2 filas, 2 columnas, 20 píxeles de espacio horizontal y vertical
+        jPanel1 = new javax.swing.JPanel();
+        botonVerLugares = new javax.swing.JButton();
+        botonPlanearActividades = new javax.swing.JButton();
+        botonInfo = new javax.swing.JButton();
+        botonNuestraFauna = new javax.swing.JButton();
+        botonMenuUsuario = new javax.swing.JButton();
+        botonEdit = new javax.swing.JButton();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
 
-        // Agregar botones al panel principal
-        panelPrincipal.add(botonVerLugares);
-        panelPrincipal.add(botonPlanearActividades);
-        panelPrincipal.add(botonInfo);
-        panelPrincipal.add(botonNuestraFauna);
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        // Agregar ActionListener a botonNuestraFauna
-        botonNuestraFauna.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                // Crear una nueva instancia de la clase Nuestra_Fauna y mostrarla
-                Nuestra_Fauna nuevaVentana = new Nuestra_Fauna();
-                nuevaVentana.setVisible(true);
-                dispose();
+        botonVerLugares.setText("Ver Lugares");
+        botonVerLugares.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botonVerLugaresActionPerformed(evt);
             }
         });
 
-        // Agregar ActionListener a botonVerLugares
-        botonVerLugares.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                // Crear una nueva instancia de la clase VerLugares y mostrarla
-                VerLugares nueva = new VerLugares();
-                nueva.setVisible(true);
-                dispose();
+        botonPlanearActividades.setText("Planear Actividades");
+        botonPlanearActividades.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botonPlanearActividadesActionPerformed(evt);
             }
         });
 
-        // Agregar ActionListener a botonPlanearActividades
-        botonPlanearActividades.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                // Crear una nueva instancia de la clase Planear y mostrarla
-                Planear n = new Planear();
+        botonInfo.setText("Información");
+        botonInfo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botonInfoActionPerformed(evt);
+            }
+        });
+
+        botonNuestraFauna.setText("Nuestra Fauna");
+        botonNuestraFauna.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botonNuestraFaunaActionPerformed(evt);
+            }
+        });
+
+        botonMenuUsuario.setText("Menú Usuario");
+        botonMenuUsuario.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botonMenuUsuarioActionPerformed(evt);
+            }
+        });
+
+        botonEdit.setText("Editar Actividades");
+        botonEdit.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botonEditActionPerformed(evt);
+            }
+        });
+
+        jLabel7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/lista-de-quehaceres.png"))); // NOI18N
+
+        jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/agregar-usuario.png"))); // NOI18N
+
+        jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/fauna-silvestre.png"))); // NOI18N
+
+        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/informacion.png"))); // NOI18N
+
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/plan-de-negocios.png"))); // NOI18N
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/lugar.png"))); // NOI18N
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(50, 50, 50)
+                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(350, 350, 350)
+                        .addComponent(botonPlanearActividades))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(350, 350, 350)
+                        .addComponent(botonNuestraFauna, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(60, 60, 60)
+                        .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(60, 60, 60)
+                        .addComponent(botonInfo, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(350, 350, 350)
+                        .addComponent(botonEdit, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(70, 70, 70)
+                        .addComponent(botonVerLugares, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(50, 50, 50)
+                        .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(340, 340, 340)
+                        .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(350, 350, 350)
+                        .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(60, 60, 60)
+                        .addComponent(botonMenuUsuario, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(350, 350, 350)
+                        .addComponent(jLabel7, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(65, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(50, 50, 50)
+                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(10, 10, 10)
+                        .addComponent(botonPlanearActividades)
+                        .addGap(206, 206, 206)
+                        .addComponent(botonNuestraFauna)
+                        .addGap(46, 46, 46)
+                        .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(420, 420, 420)
+                        .addComponent(botonInfo))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(640, 640, 640)
+                        .addComponent(botonEdit))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(190, 190, 190)
+                        .addComponent(botonVerLugares))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(270, 270, 270)
+                        .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(290, 290, 290)
+                        .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(70, 70, 70)
+                        .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(640, 640, 640)
+                        .addComponent(botonMenuUsuario))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(490, 490, 490)
+                        .addComponent(jLabel7, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(21, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>                        
+
+    private void botonVerLugaresActionPerformed(java.awt.event.ActionEvent evt) {                                                
+     
+        VerLugares nueva = new VerLugares();
+        nueva.setVisible(true);
+        dispose();
+    }                                               
+
+    private void botonMenuUsuarioActionPerformed(java.awt.event.ActionEvent evt) {                                                 
+     
+    }                                                
+
+    private void botonPlanearActividadesActionPerformed(java.awt.event.ActionEvent evt) {                                                        
+        Planear n = new Planear();
                 n.setVisible(true);
                 dispose();
-            }
-        });
+    }                                                       
 
-        // Agregar ActionListener a botonInfo
-        botonInfo.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                // Crear una nueva instancia de la clase Info y mostrarla
-                Info ne= new Info();
+    private void botonInfoActionPerformed(java.awt.event.ActionEvent evt) {                                          
+        Info ne= new Info();
                 ne.setVisible(true);
                 dispose();
+    }                                         
+
+    private void botonNuestraFaunaActionPerformed(java.awt.event.ActionEvent evt) {                                                  
+        
+         Nuestra_Fauna nuevaVentana = new Nuestra_Fauna();
+         nuevaVentana.setVisible(true);
+         dispose();
+    }                                                 
+
+    private void botonEditActionPerformed(java.awt.event.ActionEvent evt) {                                          
+        
+    }                                         
+
+   
+    public static void main(String args[]) {
+    
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Pantalla_Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Pantalla_Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Pantalla_Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Pantalla_Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+     
+  
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Pantalla_Principal().setVisible(true);
             }
         });
-
-        // Establecer tamaño preferido para los botones
-        int buttonWidth = 75;
-        int buttonHeight = 35;
-
-        botonVerLugares.setPreferredSize(new Dimension(buttonWidth, buttonHeight));
-        botonPlanearActividades.setPreferredSize(new Dimension(buttonWidth, buttonHeight));
-        botonInfo.setPreferredSize(new Dimension(buttonWidth, buttonHeight));
-        botonNuestraFauna.setPreferredSize(new Dimension(buttonWidth, buttonHeight));
-
-        // Agregar panel principal al frame
-        getContentPane().add(panelPrincipal);
-
-        // Establecer propiedades del frame
-        this.setSize(400, 300);
-        this.setLocationRelativeTo(null);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
     }
+              
 
-    public static void main(String[] args) {
-        new Pantalla_Principal();
-    }
+                   
 }
+
